f463885c74c2720b0e1948becab732aa3b810aaa
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2023-08-06 14:12:51 -0300

    add: classe responsavel por parametros

--- UtilCode/parameters.py
+++ UtilCode/parameters.py
@@ -1,7 +1,16 @@
 class Parametros:
-    maxSpeed = 70
-    normalSpeed = 50
+    maxSpeed = 100
+    normalSpeed = 60
     minSpeed = 30
     
     wheel_diameter = 35 #Diametro da roda
     axle_track = 210 # Distancia entre as rodas
+    
+    normalSpeed = 60
+    blackReflection = 30
+    
+    lineReflection = 10
+    whiteReflection = 40
+    
+    R90 = 90
+    L90 = -90
